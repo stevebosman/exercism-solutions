8b95bfeb1223243e31e6440ae952ca1db1c9c351
Steve Bosman <stevebosman@gmail.com>
2024-01-14 21:37:01 +0000

    solved scala Strain - more in spirit of the README

--- scala/src/main/scala/Strain.scala
+++ scala/src/main/scala/Strain.scala
@@ -1,4 +1,15 @@
+import scala.collection.mutable.ListBuffer
+
 object Strain {
-  def keep[T](list: Seq[T], predicate: T => Boolean): Seq[T] = list.filter(predicate)
-  def discard[T](list: Seq[T], predicate: T => Boolean): Seq[T] = list.filterNot(predicate)
+  def keep[T](list: Seq[T], predicate: T => Boolean): Seq[T] = {
+    val buffer = ListBuffer[T]()
+    list.foreach(e => if (predicate(e)) buffer += e)
+    buffer.toList
+  }
+
+  def discard[T](list: Seq[T], predicate: T => Boolean): Seq[T] = {
+    val buffer = ListBuffer[T]()
+    list.foreach(e => if (!predicate(e)) buffer += e)
+    buffer.toList
+  }
 }
