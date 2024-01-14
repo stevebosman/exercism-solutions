46dbcb503641d91cb17f199696e54fb7dd031721
Steve Bosman <stevebosman@gmail.com>
2024-01-14 11:08:36 +0000

    solved scala Raindrops

--- scala/src/main/scala/Raindrops.scala
+++ scala/src/main/scala/Raindrops.scala
@@ -1,12 +1,23 @@
 object Raindrops {
   def convert(n: Int): String = {
     var result = ""
-    if (n % 3 == 0)
+
+    if (n % 3 == 0) {
       result += "Pling"
-    if (n % 5 == 0)
+    }
+
+    if (n % 5 == 0) {
       result += "Plang"
-    if (n % 7 == 0)
+    }
+
+    if (n % 7 == 0) {
       result += "Plong"
-    if (result == "") n.toString else result
+    }
+
+    if (result == "") {
+      n.toString
+    } else {
+      result
+    }
   }
 }
