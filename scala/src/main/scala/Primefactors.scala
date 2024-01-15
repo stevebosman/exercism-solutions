fc76eb073a1eea6925c55b2b58de895e18b9a867
Steve Bosman <stevebosman@gmail.com>
2024-01-15 22:00:41 +0000

    solved scala PrimeFactors - attempt 3

--- scala/src/main/scala/Primefactors.scala
+++ scala/src/main/scala/Primefactors.scala
@@ -1,14 +1,10 @@
 import scala.annotation.tailrec
-import scala.collection.mutable.ListBuffer
 
 object PrimeFactors {
   @tailrec
-  def factors(n: Long, d: Int = 2, list: ListBuffer[Int] = ListBuffer()): List[Int] = n match {
-    case 1 => list.toList
-    case n if (n % d == 0) =>
-      list.addOne(d)
-      factors(n/d, d, list)
-    case _ =>
-      factors(n, d+1, list)
+  def factors(n: Long, d: Int = 2, list: List[Int] = List()): List[Int] = n match {
+    case 1 => list
+    case n if (n % d == 0) => factors(n/d, d, list :+ d)
+    case _ => factors(n, d+1, list)
   }
 }
