3f545bae326fb5c71d1336b1e6d21b0da466e559
Steve Bosman <stevebosman@gmail.com>
2024-01-17 23:15:27 +0000

    solved scala PascalsTriangle - use iterate and tupled

--- scala/src/main/scala/PascalsTriangle.scala
+++ scala/src/main/scala/PascalsTriangle.scala
@@ -1,13 +1,7 @@
-import scala.collection.mutable.ListBuffer
-
+import Function.tupled
 object PascalsTriangle {
-  private def row(n: Int, previous: List[Int]): List[Int] =
-    (previous :+ 0).zip(0 :: previous).map(e => e._1 + e._2)
-
-  def rows(n: Int): List[List[Int]] = {
-    if (n <= 0) return List()
-    val buff = ListBuffer[List[Int]]()
-    (1 to n).foreach(i => buff += (if (i == 1) List(1) else row(i, buff.last)))
-    buff.toList
-  }
+  def rows(n: Int): List[List[Int]] =
+    List.iterate(List(1), n) {
+      row => 0 +: row zip row :+ 0 map tupled { _ + _ }
+    }
 }
