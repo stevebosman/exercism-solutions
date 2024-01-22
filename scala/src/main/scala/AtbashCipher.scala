b1fa96dab02e0fac43b1857b030b0a7fab22f02a
Steve Bosman <stevebosman@gmail.com>
2024-01-22 22:20:34 +0000

    solved scala AtbashCipher

--- scala/src/main/scala/AtbashCipher.scala
+++ scala/src/main/scala/AtbashCipher.scala
@@ -1,17 +1,10 @@
-import scala.util.matching.Regex
-
 object AtbashCipher {
-  private val encoderPattern: Regex = """.{1,5}""".r
-
-  def encode(str: String): String = {
-    encoderPattern.findAllIn(decode(str)).mkString(" ")
-  }
-
   def decode(str: String): String =
-    str.toLowerCase.filter(c => c.isDigit || c.isLetter).map(encodeChar)
+    str.toLowerCase.filter(c => c.isLetterOrDigit).map(encodeChar)
 
-  private def encodeChar(c: Char): Char = c match {
-    case c if c.isDigit => c
-    case c => ('a' + 'z' - c).asInstanceOf[Char]
-  }
+  def encode(str: String): String =
+    decode(str).grouped(5).mkString(" ")
+
+  private def encodeChar(c: Char): Char =
+    if (c.isDigit) c else ('a' + 'z' - c).toChar
 }
