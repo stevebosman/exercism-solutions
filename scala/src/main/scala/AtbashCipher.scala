4cc121a3351c1b3a36c6300ad798c230d7c1f636
Steve Bosman <stevebosman@gmail.com>
2024-01-22 22:22:38 +0000

    solved scala AtbashCipher

--- scala/src/main/scala/AtbashCipher.scala
+++ scala/src/main/scala/AtbashCipher.scala
@@ -1,9 +1,9 @@
 object AtbashCipher {
   def decode(str: String): String =
-    str.toLowerCase.filter(c => c.isLetterOrDigit).map(encodeChar)
+    str.filter(c => c.isLetterOrDigit).map(encodeChar)
 
   def encode(str: String): String =
-    decode(str).grouped(5).mkString(" ")
+    decode(str.toLowerCase).grouped(5).mkString(" ")
 
   private def encodeChar(c: Char): Char =
     if (c.isDigit) c else ('a' + 'z' - c).toChar
