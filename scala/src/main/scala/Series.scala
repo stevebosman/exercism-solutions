db7d93842290702a00e540ef428d4f46125dbac1
Steve Bosman <stevebosman@gmail.com>
2024-01-30 21:50:54 +0000

    solve scala Series 2

--- scala/src/main/scala/Series.scala
+++ scala/src/main/scala/Series.scala
@@ -1,11 +1,4 @@
-import scala.annotation.tailrec
-
 object Series {
-  private def toDigitsList(str: String): List[Int] =
-    str.map(_.toInt - '0').toList
-
-  @tailrec
-  def slices(length: Int, str: String, list: List[List[Int]] = List()): List[List[Int]] =
-    if (str.length < length) list
-    else slices(length, str.tail, list :+ toDigitsList(str.substring(0, length)))
+  def slices(length: Int, str: String): Seq[Seq[Int]] =
+    str.map(_.toInt - '0').sliding(length).toList
 }
