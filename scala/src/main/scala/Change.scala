fa73e8589939e4decf104815106196710c3a7630
Steve Bosman <stevebosman@gmail.com>
2024-02-16 21:49:51 +0000

    solve scala Change - nicer looking, but takes 6 times longer than iteration 1

--- scala/src/main/scala/Change.scala
+++ scala/src/main/scala/Change.scala
@@ -1,29 +1,14 @@
 object Change {
-  private def bestCoins(total: Int, coins: List[Int]): List[Int] = {
-    if (total == 0)
-      List()
-    else if (coins.contains(total))
-      List(total)
-    else {
-      var best: List[Int] = Nil
-      for (coin <- coins) {
-        if (best == Nil || best.size > total / coin) {
-          if (total % coin == 0) {
-            best = List.fill(total / coin)(coin)
-          } else if (coin <= total) {
-            val potential = bestCoins(total - coin, coins) :+ coin
-            if (potential.sum == total && (best == Nil || potential.size < best.size))
-              best = potential
-          }
-        }
-      }
-      best
-    }
-  }
-
   def findFewestCoins(total: Int, coins: List[Int]): Option[List[Int]] = {
-    val best = bestCoins(total, coins.sortWith((a, b) => a > b))
-    if (best.sum != total) None
-    else Some(best)
+    if (total == 0)
+      Some(List())
+    else {
+      val min = coins.min
+      val expanded = coins.flatMap(i => List.fill(total/i)(i))
+      for (elem <- 1 to total / min)
+        for (list <- expanded.combinations(elem))
+          if (list.sum == total) return Some(list)
+      None
+    }
   }
 }
