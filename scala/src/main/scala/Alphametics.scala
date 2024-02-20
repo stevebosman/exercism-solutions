f54c236383e9464fd274e947709a95ecc7b112a0
Steve Bosman <stevebosman@gmail.com>
2024-02-20 00:08:19 +0000

    solve scala Alphametics - faster but still not fast enough

--- scala/src/main/scala/Alphametics.scala
+++ scala/src/main/scala/Alphametics.scala
@@ -1,25 +1,37 @@
-object Alphametics {
+import scala.collection.mutable
 
-  def replaceChar(c: Char, lookup: Map[Char, Int]): Char = lookup.get(c) match {
-    case Some(d) => ('0' + d).toChar
-    case _ => c
+case class Puzzle(left: Seq[String], right: String, chars: Seq[Char]) {
+  private val bannedZero: Seq[Char] = (left.map(s => s.head) :+ right.head).distinct
+
+  private def eval(lookup: Map[Char, Char]): Boolean = {
+    val memo: mutable.Map[String, Long] =
+      mutable.Map().withDefault(_.map(lookup).toLong)
+
+    left.map(memo).sum == memo(right)
   }
 
-  def eval(value: String): Boolean = {
-    if (value.matches("^(0|.* 0)\\d.*$")) return false
-    val lr = value.split("==")
-    lr(0).split("\\+").map(_.trim.toLong).sum == lr(1).trim.toLong
+  private def tryPermutation(permutation: IndexedSeq[Char]): Option[Map[Char, Int]] = {
+    val lookup: Map[Char, Char] = chars.zip(permutation).map(e => e._1 -> e._2).toMap
+    if (bannedZero.exists(c => lookup(c) == '0')) None
+    else if (eval(lookup)) Some(lookup.map(e => e._1 -> (e._2 - '0')))
+    else None
   }
 
-  def solve(puzzle: String): Option[Map[Char, Int]] = {
-    val chars: List[Char] = puzzle.filter(_.isLetter).distinct.sorted.toList
-    for (combo <- (0 to 9).combinations(chars.length)) {
-      for (digits <- combo.permutations) {
-        val lookup: Map[Char, Int] = chars.zip(digits).map(e => e._1 -> e._2).toMap
-        if (eval(puzzle.map(replaceChar(_, lookup)))) return Some(lookup)
+  def solve: Option[Map[Char, Int]] = {
+    for (combo <- ('0' to '9').combinations(chars.length)) {
+      for (perms <- combo.permutations) {
+        val value = tryPermutation(perms)
+        if (value.nonEmpty) return value
       }
     }
     None
   }
+}
 
+object Alphametics {
+  def solve(puzzle: String): Option[Map[Char, Int]] = {
+    val chars: List[Char] = puzzle.filter(_.isLetter).distinct.sorted.toList
+    val lr = puzzle.replaceAll(" ", "").split("==")
+    Puzzle(lr(0).split("\\+"), lr(1), chars).solve
+  }
 }
